adec70e5d509672e3be638e28a43aebbfeb1e138
Simon Meggle <simon.meggle@elabit.de>
2020-01-23 20:37:21 +0000

    check_parameters_robotmk.py: WATO 1st draft

--- check_parameters_robotmk.py
+++ check_parameters_robotmk.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import (
-    Checkbox,
     DropdownChoice,
     Dictionary,
-    ListChoice,
     ListOf,
     TextAscii,
-    TextUnicode,
     Tuple,
 )
 
@@ -15,11 +12,12 @@ from cmk.gui.plugins.wato import (
     CheckParameterRulespecWithItem,
     rulespec_registry,
     RulespecGroupCheckParametersDiscovery,
-    RulespecGroupCheckParametersStorage,
+    RulespecGroupCheckParametersApplications,
     HostRulespec,
 )
 
-def _valuespec_inventory_robot_rules():
+
+def _valuespec_inventory_robotmk_rules():
     return Dictionary(
         title=_("Robot Framework Service Discovery"),
         elements=[
@@ -40,29 +38,179 @@ def _valuespec_inventory_robot_rules():
                         ( "3"  , _("Directory level 3 (\"-\")") ),
                     ]
             )),
+            # TODO: Service Prefix
         ],
     )
 
 
 rulespec_registry.register(
     HostRulespec(
+        # lib/python/cmk/gui/watolib/rulespecs.py
         group=RulespecGroupCheckParametersDiscovery,
         match_type="dict",
-        name="inventory_robot_rules",
-        valuespec=_valuespec_inventory_robot_rules,
+        name="inventory_robotmk_rules",
+        valuespec=_valuespec_inventory_robotmk_rules,
     ))
 
+def _item_spec_robotmk():
+    return TextAscii(title=_("Services"),
+                     help=_("Matches the service names generated from <u>Robot suites</u>. By default this is always the <i>topmost</i> suite (level 0) which results in <i>one service</i>.<br> "
+                            "Robot suites can be nested; to define a lower level CMK should "
+                            "generate services from, use the service discovery rule "
+                            "<i>Robot Framework Service Discovery</i>.<br>"))
 
-#        elements = [
-#            ( "discovery_suite_level",
-#                DropdownChoice(
-#                    title = _("discovery_suite_level"),
-#                    choices = [
-#                        ( "0"  , _("0 - Dirname of the test -> one serviceX") ),
-#                        ( "1"  , _("1 - Names of all dirs and tests in the root folder -> many services") ),
-#                        ( "2"  , _("2 - Names of all dirs and tests in the layer 1 subfolder -> many services") ),
-#                    ]
-#            ),
-#        ]
-#
-#    ),
+def _parameter_valuespec_robotmk():
+    return Dictionary(elements=[
+        ("output_depth", Dictionary(  # L1 
+            title = _('Output depth'),
+            help = _('Configure the output depth'),
+            elements = [        
+                ("output_depth_suites", ListOf(  # /L2
+                    Tuple(  # L3
+                        title = _('<b>Suite</b> Output depth'),
+                        show_titles=True,
+                        orientation="horizontal",
+                        elements = [
+                            TextAscii(
+                                title=("<b>Suite</b> pattern"),
+                                allow_empty=False,
+                            ),
+                            Integer(
+                                title=("depth"),
+                                allow_empty=False,
+                                size=3,
+                            ),                            
+                        ],
+                    ),  # L3 / Tuple
+                    add_label=_("Add"),
+                    movable=False,
+                    title=_("<b>Suite</b> Output depth")
+                )), # L2 / output_depth_suites 
+                ("output_depth_keywords", ListOf(  # /L2
+                    Tuple(  # L3
+                        title = _('<b>Keyword</b> Output depth'),
+                        show_titles=True,
+                        orientation="horizontal",
+                        elements = [
+                            TextAscii(
+                                title=("<b>Keyword</b> pattern"),
+                                allow_empty=False,
+                            ),
+                            Integer(
+                                title=("depth"),
+                                allow_empty=False,
+                                size=3,
+                            ),                            
+                        ],
+                    ),  # L3 / Tuple
+                    add_label=_("Add"),
+                    movable=False,
+                    title=_("<b>Keyword</b> Output depth")
+                )), # L2 / output_depth_suites                                                   
+            ],
+        )), # L1 / output_depth   
+
+
+
+        ("runtime_threshold", Dictionary(
+            title = _('Runtime thresholds'),
+            help = _('Set runtime thresholds FIXME '),
+            elements = [
+                ("runtime_threshold_suites", ListOf(  # /L2
+                    Tuple(  # L3
+                        title = _('<b>Suite</b> thresholds'),
+                        show_titles=True,
+                        orientation="horizontal",
+                        elements = [
+                            TextAscii(
+                                title=("<b>Suite</b> pattern"),
+                                allow_empty=False,
+                            ),
+                            Integer(
+                                title=("WARN threshold (s)"),
+                                allow_empty=False,
+                                size=3,
+                            ),                            
+                        ],
+                    ),  # L3 / Tuple
+                    add_label=_("Add"),
+                    movable=False,
+                    title=_("<b>Suite</b> thresholds")
+                )), # L2 
+               ("runtime_threshold_tests", ListOf(  # /L2
+                    Tuple(  # L3
+                        title = _('<b>Test</b> thresholds'),
+                        show_titles=True,
+                        orientation="horizontal",
+                        elements = [
+                            TextAscii(
+                                title=("<b>Test</b> pattern"),
+                                allow_empty=False,
+                            ),
+                            Integer(
+                                title=("WARN threshold (s)"),
+                                allow_empty=False,
+                                size=3,
+                            ),                            
+                        ],
+                    ),  # L3 / Tuple
+                    add_label=_("Add"),
+                    movable=False,
+                    title=_("<b>Test</b> thresholds")
+                )), # L2                 
+                ("runtime_threshold_keywords", ListOf(  # /L2
+                    Tuple(  # L3
+                        title = _('<b>Keyword</b> thresholds'),
+                        show_titles=True,
+                        orientation="horizontal",
+                        elements = [
+                            TextAscii(
+                                title=("<b>Keyword</b> pattern"),
+                                allow_empty=False,
+                            ),
+                            Integer(
+                                title=("WARN threshold (s)"),
+                                allow_empty=False,
+                                size=3,
+                            ),                            
+                        ],
+                    ),  # L3 / Tuple
+                    add_label=_("Add"),
+                    movable=False,
+                    title=_("<b>Keyword</b> thresholds")
+                )), # L2                                       
+            ],
+        )), # L1 / runtime_threshold  
+
+
+        # TODO: Helper function comma delimited ??
+        ("perfdata_creation", Dictionary(
+            title = _('Perfdata creation'),
+            help = _('Configure FIXME Main graph & subgraphs'),
+            elements = [
+                ("perfdata_creation_suites", ListOfStrings(  # /L2
+                    title = _('<b>Suite</b> perfdata'),
+                    orientation="horizontal",
+                )), # L2 
+
+               ("perfdata_creation_tests", ListOfStrings(  # /L2
+                    title = _('<b>Test</b> perfdata'),
+                    orientation="horizontal",
+                )), # L2                
+                ("perfdata_creation_keywords", ListOfStrings(  # /L2
+                    title = _('<b>Keyword</b> perfdata'),
+                    orientation="horizontal",
+                )), # L2                                         
+            ],
+        )), # L1 / perfdata_creation                          
+    ],)
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="robotmk",
+        # gui/plugins/wato/utils/__init__.py
+        group=RulespecGroupCheckParametersApplications,
+        item_spec=_item_spec_robotmk,
+        parameter_valuespec=_parameter_valuespec_robotmk,
+        title=lambda: _("Robot Framework"),
+    ))
