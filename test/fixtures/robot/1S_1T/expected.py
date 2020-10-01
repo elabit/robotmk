74f6ad9a1c2ed5d8775b5fab9cbd4e1b248899dd
Simon Meggle <simon.meggle@elabit.de>
2020-10-02 00:08:57 +0200

    Added explanations to expected.py files so that even myself can understand it after months.

--- test/fixtures/robot/1S_1T/expected.py
+++ test/fixtures/robot/1S_1T/expected.py
@@ -1,3 +1,12 @@
+#   List of dicts for DSL 0,1,2...
+#       inventory_suites: list of Suite names the inventory function should find
+#           check_suites: The name of the item to be checked by the check (see Argument #4 in 
+#           dict 'check_test_params' in front of the check test function
+#               checkgroup_parameters file in test/fixtures/checkgroup_parameters (without .py extension), 
+#               can containing anything which can be set in the check's WATO page
+#                   svc_status: The expected Nagios state of the suite
+#                   svc_output: A Regex which is expected to match the Output  
+
 [
     # discovery_suite_level 0
     {
