335c09fbabcaabd3120a7ffd96f79c461dc18e71
Simon <simon.meggle@elabit.de>
2021-06-14 19:30:44 +0200

    Version bump: v1.1.0

--- checks/v2/robotmk.py
+++ checks/v2/robotmk.py
@@ -32,7 +32,7 @@ from cmk.utils.exceptions import MKGeneralException
 
 # UTC = pytz.utc
 
-ROBOTMK_VERSION = 'v1.1.0-beta.5'
+ROBOTMK_VERSION = 'v1.1.0'
 
 DEFAULT_SVC_PREFIX = 'Robot Framework E2E $SUITEID$SPACE-$SPACE'
 
