34109b0869719df2149d68fc7300a82c6d05cc73
Simon <simon.meggle@elabit.de>
2021-07-25 10:34:56 +0200

    V1 Devcontainer: use lsyncd instead of symlinks

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
+# This step ties the workspace files with the Devcontainer.
+# V1 sites use lsyncd instead of symlinks because the bakery/rpmbuild needs real files instead of links. 
 .devcontainer/linkfiles.sh
+# Fire up the site
 omd start
-
-SECRET=$(cat /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret)
-HOST=localhost:5000
-SITE=cmk
-
-curl -k "http://$HOST/$SITE/check_mk/webapi.py?action=add_host&_username=automation&_secret=$SECRET&request_format=python&output_format=python" -d "request={'hostname': 'win10simdows', 'folder': '', 'attributes': {'ipaddress': '192.168.116.8'}, 'create_folders': '1'}"
-cmk -IIv win10simdows
-cmk -R
