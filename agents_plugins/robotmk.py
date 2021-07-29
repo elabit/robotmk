6417e874d683455ee962ca2a59c716d247729e82
Simon <simon.meggle@elabit.de>
2021-07-29 16:50:14 +0200

    Version bump: v1.1.1

--- agents_plugins/robotmk.py
+++ agents_plugins/robotmk.py
@@ -45,7 +45,7 @@ from enum import Enum
 
 
 local_tz = datetime.utcnow().astimezone().tzinfo
-ROBOTMK_VERSION = 'v1.1.1-beta'
+ROBOTMK_VERSION = 'v1.1.1'
 
 #<robotmk-keywordlibrary
 # Imported from https://raw.githubusercontent.com/simonmeggle/robotframework-robotmk/
