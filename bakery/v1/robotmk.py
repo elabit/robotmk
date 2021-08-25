4a4a37113b641b9565340078b46dcdc736d7e1cb
Simon <simon.meggle@elabit.de>
2021-08-25 11:50:34 +0200

    Version bump: v1.2-beta

--- bakery/v1/robotmk.py
+++ bakery/v1/robotmk.py
@@ -18,7 +18,7 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-ROBOTMK_VERSION = 'v1.1.1'
+ROBOTMK_VERSION = 'v1.2-beta'
 
 import cmk.utils.paths
 import os
