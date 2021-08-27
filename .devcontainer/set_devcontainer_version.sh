ef4a3d3e840eeeb4ef23e41f66e36c52afbbc411
Simon <simon.meggle@elabit.de>
2021-08-27 15:28:17 +0200

    Devcontainer settings

--- .devcontainer/set_devcontainer_version.sh
+++ .devcontainer/set_devcontainer_version.sh
@@ -8,9 +8,9 @@ cp .devcontainer/devcontainer{_v$1,}.json
 echo "+ Setting Python version for VS Code... "
 
 if [ $VERSION -eq "1" ]; then 
-    sed -i -e 's#cmk/bin/python3"#cmk/bin/python"#' .vscode/settings.json 
+    sed -i 's#cmk/bin/python3"#cmk/bin/python"#' .vscode/settings.json 
 else 
-    sed -i -e 's#cmk/bin/python"#cmk/bin/python3"#' .vscode/settings.json 
+    sed -i 's#cmk/bin/python"#cmk/bin/python3"#' .vscode/settings.json 
 fi
 
 echo "Preparation for Checkmk version $VERSION finished. You can now start the devcontainer in VS Code with 'Remote-Containers: Rebuild Container'."
