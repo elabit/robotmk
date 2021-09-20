6b39aa55e48b45aaa314872b45c5ea1a86e828af
Simon <simon.meggle@elabit.de>
2021-09-20 23:14:40 +0200

    Devcontainer setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,16 +4,16 @@
 /workspaces/robotmk/.devcontainer/linkfiles.sh
 
 echo "Extracting automation user info ... "
-tar -C /opt/omd/sites/cmk/var/check_mk/web/ -xzf /workspaces/robotmk/.devcontainer/automation.tgz
+# tar -C /opt/omd/sites/cmk/var/check_mk/web/ -xzf /workspaces/robotmk/.devcontainer/automation.tgz
 
-# AUT_SECRET_DIR=/opt/omd/sites/cmk/var/check_mk/web/automation/
+AUT_SECRET_DIR=/opt/omd/sites/cmk/var/check_mk/web/automation/
 
-# # Password for the automation user
-# mkdir -p $AUT_SECRET_DIR
-# chmod 770 $AUT_SECRET_DIR
-# echo "secret" > $AUT_SECRET_DIR/automation.secret
-# chmod 660 $AUT_SECRET_DIR/automation.secret
-# chown -R cmk:cmk $AUT_SECRET_DIR
+# Password for the automation user
+mkdir -p $AUT_SECRET_DIR
+chmod 770 $AUT_SECRET_DIR
+echo "secret" > $AUT_SECRET_DIR/automation.secret
+chmod 660 $AUT_SECRET_DIR/automation.secret
+chown -R cmk:cmk $AUT_SECRET_DIR
 
 # Create the folder to source agent output for easy debugging.
 # Sync is done by lsyncd in linkfiles.sh.
