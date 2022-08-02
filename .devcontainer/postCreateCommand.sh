ee80779c7ea87046540591a3901284f4e0c3aa7b
Simon Meggle <simon.meggle@elabit.de>
2022-08-02 08:20:54 +0200

    Fixed postCreateCommand (disable EC, enable WebAPI, Dummyhost creation)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-echo "WORKSPACE: $WORKSPACE"
+echo "▹ WORKSPACE: $WORKSPACE"
 # This step ties the workspace files with the Devcontainer. lsyncd is used to synchronize files. 
-echo "Connecting the project files into the container..."
+echo "▹ Linking the project files into the container (linkfiles.sh)..."
 /workspaces/robotmk/.devcontainer/linkfiles.sh
 
-echo "Extracting automation user info ... "
-# tar -C /opt/omd/sites/cmk/var/check_mk/web/ -xzf /workspaces/robotmk/.devcontainer/automation.tgz
+# Create the folder to source agent output for easy debugging.
+# Sync is done by lsyncd in linkfiles.sh.
+mkdir -p $OMD_ROOT/var/check_mk/agent_output
+chown -R cmk:cmk $OMD_ROOT/var/check_mk/agent_output
 
-AUT_SECRET_DIR=/opt/omd/sites/cmk/var/check_mk/web/automation/
-
-# Password for the automation user
+echo "▹ Setting automation user secret to 'secret' ... "
+AUT_SECRET_DIR=$OMD_ROOT/var/check_mk/web/automation/
 mkdir -p $AUT_SECRET_DIR
 chmod 770 $AUT_SECRET_DIR
 echo "secret" > $AUT_SECRET_DIR/automation.secret
 chmod 660 $AUT_SECRET_DIR/automation.secret
 chown -R cmk:cmk $AUT_SECRET_DIR
 
-# Create the folder to source agent output for easy debugging.
-# Sync is done by lsyncd in linkfiles.sh.
-mkdir -p /omd/sites/cmk/var/check_mk/agent_output
-chown -R cmk:cmk /omd/sites/cmk/var/check_mk/agent_output
+echo "▹ Disabling the EC..."
+sed -i '/mkeventd_enabled/d' $OMD_ROOT/etc/check_mk/conf.d/mkeventd.mk
+echo "mkeventd_enabled = False" >> $OMD_ROOT/etc/check_mk/conf.d/mkeventd.mk
+sed -i '/mkeventd_enabled/d' $OMD_ROOT/etc/check_mk/multisite.d/wato/global.mk
+echo "mkeventd_enabled = False" >> $OMD_ROOT/etc/check_mk/multisite.d/wato/global.mk
 
-# Fire up the site
-omd start
 
-# Create localhost
+echo "▹ Disabling the Liveproxyd..."
+sed -i '/liveproxyd_enabled/d' $OMD_ROOT/etc/check_mk/multisite.d/mkeventd.mk
+echo "liveproxyd_enabled = False" >> $OMD_ROOT/etc/check_mk/multisite.d/mkeventd.mk
+
+echo "▹ Enabling the Web API..."
+sed -i '/disable_web_api/d' $OMD_ROOT/etc/check_mk/multisite.d/wato/global.mk
+echo "disable_web_api = False" >> $OMD_ROOT/etc/check_mk/multisite.d/wato/global.mk
+
+echo "▹ Starting OMD... "
+omd restart
+
+echo "▹ Creating localhost via Web API..."
 /workspaces/robotmk/.devcontainer/create_dummyhost.sh
+
+echo "✅ postCreateCommand.sh finished."
