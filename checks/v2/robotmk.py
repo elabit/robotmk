4914a6695059ad718b0751b6343aa3954ed058fa
Simon Meggle <simon.meggle@elabit.de>
2022-08-23 15:21:35 +0200

    Version bump: v1.2.10-beta

--- checks/v2/robotmk.py
+++ checks/v2/robotmk.py
@@ -35,7 +35,7 @@ from collections import namedtuple
 from cmk.base.plugins.agent_based.agent_based_api.v1 import *
 from cmk.utils.exceptions import MKGeneralException
 
-ROBOTMK_VERSION = 'v1.2.9'
+ROBOTMK_VERSION = 'v1.2.10-beta'
 DEFAULT_SVC_PREFIX = 'Robot Framework E2E $SUITEID$SPACE-$SPACE'
 HTML_LOG_DIR = "%s/%s" % (os.environ['OMD_ROOT'], 'var/robotmk')
 
