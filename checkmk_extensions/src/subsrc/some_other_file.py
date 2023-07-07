1c2e8fa8d7c67c58b1152c0703cc46b0cae9f9b7
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-07-07 16:13:31 +0200

    mypy: turn typing on in package robotmk

--- checkmk_extensions/src/subsrc/some_other_file.py
+++ checkmk_extensions/src/subsrc/some_other_file.py
@@ -2,7 +2,7 @@
 
 from cmk.base.plugins.agent_based.agent_based_api.v1 import State
 
-from robotmk.config.config import DIR_SUBPATHS  # type: ignore[import]
+from robotmk.config.config import DIR_SUBPATHS
 
 
 def _state() -> State:
