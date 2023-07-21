abf28b4acabb9aad2922a40ff922e046518fed48
Luka Racic <luka.racic@checkmk.com>
2023-07-21 16:22:58 +0200

    scheduler_runner: throw errors right away

--- checkmk_extensions/agent/scheduler_runner.ps1
+++ checkmk_extensions/agent/scheduler_runner.ps1
@@ -4,27 +4,24 @@ Set-StrictMode -Version 3.0
 class Config {
     [bool]$rcc
 
-    static [string] GetConfigContent($configFilePath, [ref]$errorCollection) {
+    static [string] GetConfigContent($configFilePath) {
         try {
             $configFileContent = Get-Content -Raw -Path $configFilePath -ErrorAction Stop
             return $configFileContent
         }
         catch {
-            # Add the error message to the error collection
-            $errorCollection.Value.Add($_.Exception.Message)
-            return $null
+            throw
         }
     }
 
     # Method to parse the config file (JSON) and set the value of rcc
     static [Config] ParseConfigFile($configFilePath) {
         $config = [Config]::new()
-        $errors = [System.Collections.Generic.List[string]]::new()
 
         try {
-            $configFileContent = [Config]::GetConfigContent($configFilePath, ([ref]$errors))
+            $configFileContent = [Config]::GetConfigContent($configFilePath)
             if ($null -ne $configFileContent -and $configFileContent -ne '') {
-                $configData = $configFileContent | ConvertFrom-Json
+                $configData = $configFileContent | ConvertFrom-Json -ErrorAction Stop
 
                 # Assign the value from the config file to rcc
                 $config.rcc = $configData.PSobject.Properties["rcc"].Value -as [bool]
@@ -32,16 +29,8 @@ class Config {
         }
         catch {
             Write-Host "An error occurred while parsing the config file" -ForegroundColor Red
-            # Add the error message to the error collection
-            $errors.Add($_.Exception.Message)
+            throw $_.Exception.Message
         }
-
-        # Check if there are any errors and throw them
-        if ($errors.Count -gt 0) {
-            $errorMessages = $errors -join "`n"
-            throw $errorMessages
-        }
-
         return $config
     }
 }
