41d223206aaabe7b796f2effb4907de210c717b2
Luka Racic <luka.racic@checkmk.com>
2023-07-24 13:15:50 +0200

    scheduler_runner: simplify code

--- checkmk_extensions/agent/scheduler_runner.ps1
+++ checkmk_extensions/agent/scheduler_runner.ps1
@@ -4,33 +4,26 @@ Set-StrictMode -Version 3.0
 class Config {
     [bool]$rcc
 
-    static [string] GetConfigContent($configFilePath) {
-        try {
-            $configFileContent = Get-Content -Raw -Path $configFilePath -ErrorAction Stop
-            return $configFileContent
-        }
-        catch {
-            throw
-        }
+    # Constructor
+    # This is a way to make the rcc property mandatory
+    Config([bool]$rcc) {
+        $this.rcc = $rcc
     }
 
     # Method to parse the config file (JSON) and set the value of rcc
     static [Config] ParseConfigFile($configFilePath) {
-        $config = [Config]::new()
+
+        $configFileContent = Get-Content -Raw -Path $configFilePath -ErrorAction Stop
 
         try {
-            $configFileContent = [Config]::GetConfigContent($configFilePath)
-            if ($null -ne $configFileContent -and $configFileContent -ne '') {
-                $configData = $configFileContent | ConvertFrom-Json -ErrorAction Stop
-
-                # Assign the value from the config file to rcc
-                $config.rcc = $configData.PSobject.Properties["rcc"].Value -as [bool]
-                }
+            $configData = $configFileContent | ConvertFrom-Json -ErrorAction Stop
+            # Assign the value from the config file to rcc
+            $use_rcc = $configData.rcc -as [bool]
         }
         catch {
-            Write-Host "An error occurred while parsing the config file" -ForegroundColor Red
             throw $_.Exception.Message
         }
-        return $config
+
+        return [Config]::new($use_rcc)
     }
 }
