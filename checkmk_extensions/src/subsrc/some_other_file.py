7eee44d1a43ada595e308d8e94abe8e41164ef42
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-08-01 13:03:21 +0200

    Remove `robotmk` package from ci
    We have started over in the `v2` directory, and are keeping the
    directory as a readable reference only.

--- checkmk_extensions/src/subsrc/some_other_file.py
+++ checkmk_extensions/src/subsrc/some_other_file.py
@@ -2,12 +2,6 @@
 
 from cmk.base.plugins.agent_based.agent_based_api.v1 import State
 
-from robotmk.config.config import DIR_SUBPATHS
-
 
 def _state() -> State:
     return State.OK
-
-
-def whatever() -> str:
-    return str(DIR_SUBPATHS["cfgdir"])
