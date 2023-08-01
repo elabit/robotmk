96edf5e1f407be2ba3eba57c8617f7222a7e7749
Luka Racic <luka.racic@checkmk.com>
2023-08-01 11:57:42 +0200

    scheduler_runner: Create Command class

--- checkmk_extensions/agent/scheduler_runner.ps1
+++ checkmk_extensions/agent/scheduler_runner.ps1
@@ -27,25 +27,32 @@ class Config {
         return [Config]::new($use_rcc)
     }
 }
-function CreateCommand([Config]$config) {
-    $parentDir = Split-Path $PSScriptRoot -Parent
 
-    if ($config.rcc) {
-        $rccPath = Join-Path $PSScriptRoot "rcc.exe"
-        $robotPath = Join-Path $parentDir "config\robot.yaml"
+class Command {
+    [string]$Executable
+    [System.Collections.Generic.List[string]]$ArgumentsList
 
-        $argumentsList = "run --robot $robotPath"
-    }
-    else {
-        $pythonExe = (Get-Command python).Source
-        $moduleName = "robotmk.scheduler"
-        $configPath = Join-Path $parentDir "config\robotmk.json"
-
-        $argumentsList = "-m $moduleName --config $configPath"
+    Command([string]$executable, [System.Collections.Generic.List[string]]$argumentsList) {
+        $this.Executable = $executable
+        $this.ArgumentsList = $argumentsList
     }
 
-    return [PSCustomObject]@{
-        Executable = if ($config.rcc) { $rccPath } else { $pythonExe }
-        Arguments = $argumentsList
+    # Method to create the command depending on the rcc value
+    static [Command] CreateCommand([Config]$config) {
+        $parentDir = Split-Path $PSScriptRoot -Parent
+
+        if ($config.rcc) {
+            $_executable = Join-Path $PSScriptRoot "rcc.exe"
+            $robotPath = Join-Path $parentDir "config\robot.yaml"
+            $_argumentsList = @("run", "--robot", $robotPath)
+        }
+        else {
+            $_executable = (Get-Command python3).Source
+            $moduleName = "robotmk.scheduler"
+            $configPath = Join-Path $parentDir "config\robotmk.json"
+            $_argumentsList = @("-m", $moduleName, "--config", $configPath)
+        }
+
+        return [Command]::new($_executable, $_argumentsList)
     }
 }
