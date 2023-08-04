0e8df961e67fdf325e7334ca557e4c1e2309c615
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-08-04 10:58:54 +0200

    Allows to run scheduler with config in vagrant machine
    * Add python
    * Add and configure poetry
    * Remove `poetry.toml
    
    Execution can be done by invoking
    ```
    powershell v2\data\retry_rcc\windows.ps1
    ```
    
    The `poetry.toml` needed to be deleted, since we could not find a way
    for poetry to ignore it. This in turn causes it to create the virtual
    environment inside of the mounted directory. This causes the virtual
    machine to crash.
    
    CMK-14095

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -1,4 +1,5 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y googlechrome
+choco install -y python
 
 (new-object net.webclient).DownloadFile("https://downloads.robocorp.com/rcc/releases/latest/windows64/rcc.exe", "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe")
