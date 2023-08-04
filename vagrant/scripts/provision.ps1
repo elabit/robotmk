4e30c3120af52dffa9f93eecd0cbaa4775a2b70a
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-08-04 15:19:35 +0200

    Add nushell to vagrant
    Easier navigation on Windows host

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -1,5 +1,6 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y googlechrome
 choco install -y python
+choco install -y nushell
 
 (new-object net.webclient).DownloadFile("https://downloads.robocorp.com/rcc/releases/latest/windows64/rcc.exe", "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe")
