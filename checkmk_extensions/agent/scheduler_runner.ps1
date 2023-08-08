a605bb420a6e6a890236199731670fc07466505e
Luka Racic <104823464+racicLuka@users.noreply.github.com>
2023-08-08 15:42:46 +0200

    scheduler_runner: StartSchedulerRunner (#291)
    * scheduler_runner: StartSchedulerRunner function
    
    Introduce StartSchedulerRunner function
    
    * scheduler_runner: StartSchedulerRunner
    
    Transform StartSchedulerRunner into a [CmdletBinding()] function
    
    * scheduler_runner: Added basic logging
    
    Added basic logging for the StartSchedulerRunner function
    Log file is name `scheduler_runner.log`
    
    * scheduler_runner: Start process inside while loop

--- checkmk_extensions/agent/scheduler_runner.ps1
+++ checkmk_extensions/agent/scheduler_runner.ps1
@@ -56,3 +56,86 @@ class Command {
         return [Command]::new($_executable, $_argumentsList)
     }
 }
+
+function StartSchedulerRunner {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$configFilePath
+    )
+
+    $config = [Config]::ParseConfigFile($configFilePath)
+
+    $command = [Command]::CreateCommand($config)
+    $parentDir = Split-Path $PSScriptRoot -Parent
+    $logPath = Join-Path $parentDir "log\robotmk\scheduler_runner.log"
+
+    # Create processInfo
+    $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processInfo.FileName = $command.Executable
+    $processInfo.Arguments = $command.ArgumentsList
+    $processInfo.RedirectStandardOutput = $true
+    $processInfo.RedirectStandardError = $true
+    $processInfo.UseShellExecute = $false
+    $processInfo.CreateNoWindow = $true
+    # $processInfo.WorkingDirectory -> Using this we can configure from where the Process will be started
+    $processInfo.WorkingDirectory = $PSScriptRoot
+
+    # Create process and assign processInfo to it
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $processInfo
+
+
+    try {
+        # Read stdout and stderr and handle them as needed
+        while ($true) {
+            $process.Start() | Out-Null
+            $stdout = $process.StandardOutput.ReadLine()
+            if ($null -ne $stdout) {
+                # Handle stdout output here
+                WriteLog -Message $stdout -LogPath $logPath
+            }
+
+            $stderr = $process.StandardError.ReadLine()
+            if ($null -ne $stderr) {
+                # Handle stderr output here
+                WriteLog -Message $stderr -LogPath $logPath
+            }
+        }
+    }
+    catch {
+        # TODO: Handle errors
+        WriteLog -Message $_.Exception.Message -LogPath $logPath
+        throw $_.Exception.Message
+    }
+}
+
+function WriteLog {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Message,
+
+        [Parameter(Mandatory=$true)]
+        [string]$LogPath
+    )
+
+    # Create the log file and the parent folder if they don't exist
+    $LogFolder = Split-Path -Parent $LogPath
+    if (-not (Test-Path $LogFolder)) {
+        $null = New-Item -Path $LogFolder -ItemType Directory
+    }
+
+    if (-not (Test-Path $LogPath)) {
+        $null = New-Item -Path $LogPath -ItemType File
+    }
+
+    # Get the current timestamp
+    $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    # Format the log entry
+    $LogEntry = "$TimeStamp - $Message"
+
+    # Write the log entry to the log file
+    $LogEntry | Out-File -Append $LogPath
+}
