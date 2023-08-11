78e99e52530ef76843b584a01a10595b3cacfa0d
Joerg Herbel <joerg.herbel@checkmk.com>
2023-08-11 11:13:45 +0200

    Add session layer
    For now, we only implement running in the current session (can be either
    headed or headless). In the future, we will add the option to run in the
    session of another user.
    
    CMK-14088

---
+++ v2/robotmk/session.py
@@ -0,0 +1,19 @@
+import subprocess
+from dataclasses import dataclass
+
+from .environment import RCCEnvironment, ResultCode, RobotEnvironment
+from .runner import Attempt
+
+
+@dataclass(frozen=True)
+class CurrentSession:
+    environment: RCCEnvironment | RobotEnvironment
+
+    def run(self, attempt: Attempt) -> ResultCode:
+        return self.environment.create_result_code(
+            subprocess.run(
+                self.environment.wrap_for_execution(attempt.command),
+                check=False,
+                encoding="utf-8",
+            )
+        )
