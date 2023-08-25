96541ea0041aefe86cb81e9a260369417a12e4e0
Joerg Herbel <joerg.herbel@checkmk.com>
2023-08-25 06:52:44 +0200

    Vagrant provisioning: Install robotmk Python globally with pip

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -4,3 +4,5 @@ choco install -y python
 choco install -y nushell
 
 (new-object net.webclient).DownloadFile("https://downloads.robocorp.com/rcc/releases/v14.6.0/windows64/rcc.exe", "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe")
+
+python -m pip install -e "C:\robotmk\"
