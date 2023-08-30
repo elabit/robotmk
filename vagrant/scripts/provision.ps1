b09394e788cde27205b82629e4a56e5dd73377a0
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-08-30 11:42:35 +0200

    Fix provisioning and configuration errors
    * `windows.json` was outdated
    * vagrant machine no longer installed rcc correctly
    
    CMK-14347

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -4,7 +4,7 @@ choco install -y --force python
 choco install -y --force nushell
 
 (new-object net.webclient).DownloadFile("https://nightly.link/elabit/robotmk/actions/artifacts/878920132.zip", "C:\Users\vagrant\Downloads\rcc.zip")
-Expand-Archive "C:\Users\vagrant\Downloads\rcc.zip" -Force
-Copy-Item "C:\Users\vagrant\Downloads\rcc\windows64\rcc.exe" -Destination "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe"
+Expand-Archive "C:\Users\vagrant\Downloads\rcc.zip" -Force -DestinationPath "C:\Users\vagrant\Downloads\"
+Copy-Item "C:\Users\vagrant\Downloads\windows64\rcc.exe" -Destination "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe"
 
 python -m pip install -e "C:\robotmk\"
