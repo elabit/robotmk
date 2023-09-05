615d43fcfb6f6084157347cefe57ec2a68902ca4
Joerg Herbel <joerg.herbel@checkmk.com>
2023-09-05 14:26:06 +0200

    Collector: Introduce main function
    CMK-14435

--- checkmk_extensions/agent/robotmk_collector.ps1
+++ checkmk_extensions/agent/robotmk_collector.ps1
@@ -1,21 +1,30 @@
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
+function Main {
+    Param (
+        [parameter(Mandatory=$true, Position=0)]
+        [string]$ConfigPath
+    )
+
+    $resultsDirectory = GetResultsDirectory($ConfigPath)
+    $files = Get-ChildItem -File -Recurse $resultsDirectory
+
+    Write-Output "<<<robotmk_v2:sep(10)>>>"
+    foreach ($file in $files) {
+        Write-Output (Get-Content -Raw -ErrorAction 'SilentlyContinue' $file.FullName)
+    }
+}
+
 function GetResultsDirectory {
     [OutputType([string])]
     Param (
         [parameter(Mandatory=$true, Position=0)]
-        [string]$configPath
+        [string]$ConfigPath
     )
-    $configFileContent = Get-Content -Raw -Path $configPath
+    $configFileContent = Get-Content -Raw -Path $ConfigPath
     $configData = ConvertFrom-Json -InputObject $configFileContent
     return $configData.results_directory -as [string]
 }
 
-$resultsDirectory = GetResultsDirectory("${env:MK_CONFDIR}\robotmk.json")
-$files = Get-ChildItem -File -Recurse $resultsDirectory
-
-"<<<robotmk_v2:sep(10)>>>"
-foreach ($file in $files) {
-	Get-Content -ErrorAction 'SilentlyContinue' $file.FullName
-}
+Main("${env:MK_CONFDIR}\robotmk.json")
