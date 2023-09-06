f16d7e9ee37f468c5105b251625c539a0fa1cc0f
Joerg Herbel <joerg.herbel@checkmk.com>
2023-09-06 17:10:03 +0200

    Collector: Report configuration or configuration reading error
    CMK-14435

--- checkmk_extensions/agent/robotmk_collector.ps1
+++ checkmk_extensions/agent/robotmk_collector.ps1
@@ -6,23 +6,52 @@ function Main {
         [parameter(Mandatory=$true, Position=0)]
         [string]$ConfigPath
     )
-
-    $resultsDirectory = GetResultsDirectory($ConfigPath)
-    $files = Get-ChildItem -File -Recurse $resultsDirectory
-
     Write-Output "<<<robotmk_v2:sep(10)>>>"
+
+    try {
+        $configFileContent = Get-Content -Raw -Path $ConfigPath
+    }
+    catch {
+        Write-Output (SerializeConfigReadingError($Error[0]))
+        Throw $Error[0]
+    }
+
+    # We don't know if the config is actually valid, which is why don't simply dump it as is but
+    # instead wrap it.
+    Write-Output (SerializeConfigFileContent($configFileContent))
+
+    $files = Get-ChildItem -File -Recurse (GetResultsDirectory($configFileContent))
     foreach ($file in $files) {
         Write-Output (Get-Content -Raw -ErrorAction 'SilentlyContinue' $file.FullName)
     }
+}
+
+
+function SerializeConfigReadingError {
+    [OutputType([string])]
+    Param (
+        [parameter(Mandatory=$true, Position=0)]
+        [System.Management.Automation.ErrorRecord]$Err
+    )
+    return ConvertTo-Json -InputObject @{ config_reading_error = Out-String -InputObject $Err; }
+}
+
+
+function SerializeConfigFileContent {
+    [OutputType([string])]
+    Param (
+        [parameter(Mandatory=$true, Position=0)]
+        [string]$ConfigFileContent
+    )
+    return ConvertTo-Json -InputObject @{ config_file_content = $configFileContent; }
 }
 
 function GetResultsDirectory {
     [OutputType([string])]
     Param (
         [parameter(Mandatory=$true, Position=0)]
-        [string]$ConfigPath
+        [string]$ConfigFileContent
     )
-    $configFileContent = Get-Content -Raw -Path $ConfigPath
     $configData = ConvertFrom-Json -InputObject $configFileContent
     return $configData.results_directory -as [string]
 }
