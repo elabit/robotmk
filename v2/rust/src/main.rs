bbbd1fe51887adbdbbfd115ef2a51aa0248cbc85
Joerg Herbel <joerg.herbel@checkmk.com>
2023-09-15 10:13:57 +0200

    Rust scheduler: Set up logging according to CLI options
    CMK-14517

--- v2/rust/src/main.rs
+++ v2/rust/src/main.rs
@@ -1,9 +1,15 @@
 #![allow(dead_code)]
 pub mod attempt;
 mod cli;
+mod logging;
+
 use clap::Parser;
+use log::info;
 
 fn main() -> anyhow::Result<()> {
-    let _args = cli::Args::parse();
-    Ok(())
+    let args = cli::Args::parse();
+    logging::init(args.log_specification(), &args.log_path)?;
+    info!("Program started and logging set up");
+
+    Ok(()).map_err(logging::log_and_return_error)
 }
