c1f39967c5650bb480986e7bf9080f150b2de5f3
Joerg Herbel <joerg.herbel@checkmk.com>
2023-10-10 13:21:01 +0200

    Move process tree termination functionality to termination module
    We will later on also need this functionality when running tasks, so we
    move it to a place from where it makes sense to import it.
    
    CMK-14833

--- v2/robotmk/src/child_process_supervisor.rs
+++ v2/robotmk/src/child_process_supervisor.rs
@@ -1,14 +1,14 @@
 use super::command_spec::CommandSpec;
-use super::termination::TerminationFlag;
+use super::termination::{kill_process_tree, TerminationFlag};
+
 use anyhow::{Context, Result};
 use async_std::{future::timeout, task::sleep};
 use camino::Utf8PathBuf;
 use futures::executor;
 use log::{debug, error};
-use std::collections::{HashMap, HashSet};
 use std::process::{Child, Command, ExitStatus, Stdio};
 use std::time::Duration;
-use sysinfo::{Pid, PidExt, Process, ProcessExt, System, SystemExt};
+use sysinfo::{Pid, PidExt};
 
 pub struct ChildProcessSupervisor<'a> {
     pub command_spec: &'a CommandSpec,
@@ -47,7 +47,7 @@ impl ChildProcessSupervisor<'_> {
             Ok(child_result) => child_result,
             _ => {
                 error!("Timed out");
-                kill_process_tree(&mut child);
+                kill_child_tree(&child);
                 Ok(ChildProcessOutcome::TimedOut)
             }
         }
@@ -74,7 +74,7 @@ impl ChildProcessSupervisor<'_> {
     async fn wait_for_child_exit(&self, child: &mut Child) -> Result<ChildProcessOutcome> {
         loop {
             if self.termination_flag.should_terminate() {
-                kill_process_tree(child);
+                kill_child_tree(child);
                 return Ok(ChildProcessOutcome::Terminated);
             }
 
@@ -85,7 +85,7 @@ impl ChildProcessSupervisor<'_> {
                     child.id()
                 ))
                 .map_err(|err| {
-                    kill_process_tree(child);
+                    kill_child_tree(child);
                     err
                 })?
             {
@@ -103,43 +103,6 @@ pub enum ChildProcessOutcome {
     Terminated,
 }
 
-// This is a non-cooperative termination (SIGKILL) of the entire child process tree. What we would
-// actually like to do is to shut down our child co-operatively and leave the termination of any
-// non-direct children further down the tree to our child. However, Windows offers no API for this
-// (there is no SIGTERM on Windows), so we instead kill the entire tree.
-fn kill_process_tree(child: &mut Child) {
-    let mut system = System::new_all();
-    system.refresh_processes();
-    let _ = child.kill();
-    kill_all_children(&Pid::from_u32(child.id()), system.processes());
-}
-
-fn kill_all_children<'a>(top_pid: &'a Pid, processes: &'a HashMap<Pid, Process>) {
-    let mut pids_in_tree = HashSet::from([top_pid]);
-
-    loop {
-        let current_tree_size = pids_in_tree.len();
-        add_and_kill_direct_children(&mut pids_in_tree, processes);
-        if pids_in_tree.len() == current_tree_size {
-            break;
-        }
-    }
-}
-
-fn add_and_kill_direct_children<'a>(
-    pids_in_tree: &mut HashSet<&'a Pid>,
-    processes: &'a HashMap<Pid, Process>,
-) {
-    for (pid, parent_pid, process) in processes.iter().filter_map(|(pid, process)| {
-        process
-            .parent()
-            .map(|parent_pid| (pid, parent_pid, process))
-    }) {
-        {
-            if pids_in_tree.contains(&parent_pid) {
-                pids_in_tree.insert(pid);
-                process.kill();
-            }
-        }
-    }
+fn kill_child_tree(child: &Child) {
+    kill_process_tree(&Pid::from_u32(child.id()))
 }
