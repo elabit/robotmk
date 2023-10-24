890e8f3436a79b1bcae8079e6556bf04645851b3
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-10-24 09:58:22 +0200

    Cleanup shell provisioning script
    We don't use nushell. And our project no longer is python based.

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -1,11 +1,8 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y --force googlechrome
 choco install -y --force python
-choco install -y --force nushell
 
 (new-object net.webclient).DownloadFile("https://nightly.link/elabit/robotmk/actions/artifacts/878920132.zip", "C:\Users\vagrant\Downloads\rcc.zip")
 Expand-Archive "C:\Users\vagrant\Downloads\rcc.zip" -Force -DestinationPath "C:\Users\vagrant\Downloads\"
 Copy-Item "C:\Users\vagrant\Downloads\windows64\rcc.exe" -Destination "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe"
 rcc configure identity --do-not-track
-
-python -m pip install -e "C:\robotmk\"
