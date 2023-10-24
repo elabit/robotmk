9e246a0b01104727bef55831981c6841564b2b1b
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-10-24 09:24:39 +0200

    agent: use rust binary instead of powershell
    CMK-14887

--- vagrant/f12.sh
+++ vagrant/f12.sh
@@ -12,12 +12,10 @@ agentplugindir="$agentdir"/plugins
 agentconfigdir="$agentdir"/config
 agentbindir="$agentdir"/bin
 
-# Sync robotmk.exe to vagrant machine
+# Sync rust binaries to vagrant machine (scheduler and agent plugin)
 (cd "$rustdir"; cargo build --target=x86_64-pc-windows-gnu)
 sshpass -p "vagrant" scp -P 2222 "$rustdir"/target/x86_64-pc-windows-gnu/debug/robotmk.exe vagrant@127.0.0.1:"$agentbindir"
+sshpass -p "vagrant" scp -P 2222 "$rustdir"/target/x86_64-pc-windows-gnu/debug/agent.exe vagrant@127.0.0.1:"$agentplugindir"
 
-# Sync collector.ps1 to vagrant machine
-sshpass -p "vagrant" scp -P 2222 "$topdir"/checkmk_extensions/agent/robotmk_collector.ps1 vagrant@127.0.0.1:"$agentplugindir"
-
-# Sync config for collector.ps1 to vagrant machine
+# Sync config for agent plugin to vagrant machine
 sshpass -p "vagrant" scp -P 2222 "$topdir"/v2/data/retry_rcc/windows.json vagrant@127.0.0.1:"$agentconfigdir"/robotmk.json
