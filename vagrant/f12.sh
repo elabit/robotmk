778483eaf5e7f865877b9422827fd248ad807fe8
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-10-25 15:53:19 +0200

    Rename agent plugin
    CMK-14924

--- vagrant/f12.sh
+++ vagrant/f12.sh
@@ -15,7 +15,7 @@ agentbindir="$agentdir"/bin
 # Sync rust binaries to vagrant machine (scheduler and agent plugin)
 (cd "$rustdir"; cargo build --target=x86_64-pc-windows-gnu)
 sshpass -p "vagrant" scp -P 2222 "$rustdir"/target/x86_64-pc-windows-gnu/debug/robotmk.exe vagrant@127.0.0.1:"$agentbindir"
-sshpass -p "vagrant" scp -P 2222 "$rustdir"/target/x86_64-pc-windows-gnu/debug/agent.exe vagrant@127.0.0.1:"$agentplugindir"
+sshpass -p "vagrant" scp -P 2222 "$rustdir"/target/x86_64-pc-windows-gnu/debug/robotmk_agent.exe vagrant@127.0.0.1:"$agentplugindir"
 
 # Sync config for agent plugin to vagrant machine
 sshpass -p "vagrant" scp -P 2222 "$topdir"/v2/data/retry_rcc/windows.json vagrant@127.0.0.1:"$agentconfigdir"/robotmk.json
