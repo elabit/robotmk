424407b6d0da58b907838dba295b5a8e3436fc3d
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-11-08 19:09:02 +0100

    V: remove async-std: create generic wait
    We want to remove async-std, since it is no longer maintained.

--- v2/robotmk/src/termination.rs
+++ v2/robotmk/src/termination.rs
@@ -1,5 +1,8 @@
+use std::future::Future;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::{task::yield_now, time::sleep};
 
 #[derive(Clone)]
 pub struct TerminationFlag(Arc<AtomicBool>);
@@ -11,5 +14,29 @@ impl TerminationFlag {
 
     pub fn should_terminate(&self) -> bool {
         self.0.load(Ordering::Relaxed)
+    }
+}
+
+pub enum Outcome<T> {
+    Cancel,
+    Timeout,
+    Completed(T),
+}
+
+#[tokio::main]
+pub async fn waited<F>(duration: Duration, flag: &TerminationFlag, future: F) -> Outcome<F::Output>
+where
+    F: Future,
+{
+    async fn cancelled(flag: &TerminationFlag) {
+        while !flag.should_terminate() {
+            yield_now().await
+        }
+    }
+
+    tokio::select! {
+        outcome = future => { Outcome::Completed(outcome) },
+        _ = cancelled(flag) => { Outcome::Cancel },
+        _ = sleep(duration) => { Outcome::Timeout },
     }
 }
