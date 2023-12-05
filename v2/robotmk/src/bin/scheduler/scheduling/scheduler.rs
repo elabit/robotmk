9b494eef6dda89d26709706324276adc8adac237
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-12-05 13:32:08 +0100

    spinloop: use tokio instead of clokwerk
    This also changes the behaviour of scheduler. It will now tick fast as
    possible, until caught up, instead of emitting an error message.
    
    |     1     |     2     |     3     |     4     |     5     |     6     |
    | work -----|          delay          | work | work | work -| work -----|
    
    CMK-15137

--- v2/robotmk/src/bin/scheduler/scheduling/scheduler.rs
+++ v2/robotmk/src/bin/scheduler/scheduling/scheduler.rs
@@ -4,43 +4,54 @@ use crate::internal_config::{GlobalConfig, Suite};
 use crate::logging::log_and_return_error;
 
 use anyhow::{bail, Result};
-use clokwerk::{Scheduler, TimeUnits};
 use log::error;
-use std::thread::{sleep, spawn};
+use std::thread::sleep;
 use std::time::Duration;
+use tokio::task::spawn_blocking;
+use tokio::time::interval;
+use tokio_util::sync::CancellationToken;
 
-pub fn run_suites_and_cleanup(global_config: &GlobalConfig, suites: &[Suite]) -> Result<()> {
-    let mut scheduler = Scheduler::new();
+#[tokio::main]
+pub async fn run_suites_and_cleanup(global_config: &GlobalConfig, suites: &[Suite]) -> Result<()> {
     let suites_for_scheduling: Vec<Suite> = suites.to_vec();
 
     for suite in suites_for_scheduling {
-        scheduler
-            .every(suite.execution_interval_seconds.seconds())
-            .run(move || run_suite_in_new_thread(suite.clone()));
+        tokio::spawn(run_suite_scheduler(suite));
     }
 
-    let suites_for_cleanup: Vec<Suite> = suites.to_vec();
-    scheduler
-        .every(5.minutes())
-        .run(move || run_cleanup_working_directories_in_new_thread(suites_for_cleanup.clone()));
+    tokio::spawn(run_cleanup_job(
+        global_config.cancellation_token.clone(),
+        suites.to_vec(),
+    ));
 
+    global_config.cancellation_token.cancelled().await;
+    error!("Received termination signal while scheduling, waiting for suites to terminate");
+    wait_until_all_suites_have_terminated(suites);
+    bail!("Terminated");
+}
+
+async fn run_suite_scheduler(suite: Suite) {
+    let mut clock = interval(Duration::from_secs(suite.execution_interval_seconds));
     loop {
-        if global_config.cancellation_token.is_cancelled() {
-            error!("Received termination signal while scheduling, waiting for suites to terminate");
-            wait_until_all_suites_have_terminated(suites);
-            bail!("Terminated");
-        }
-        scheduler.run_pending();
-        sleep(Duration::from_millis(250));
+        let suite = suite.clone();
+        tokio::select! {
+            _ = clock.tick() => { }
+            _ = suite.cancellation_token.cancelled() => { return }
+        };
+        spawn_blocking(move || run_suite(&suite).map_err(log_and_return_error));
     }
 }
 
-fn run_suite_in_new_thread(suite: Suite) {
-    spawn(move || run_suite(&suite).map_err(log_and_return_error));
-}
-
-fn run_cleanup_working_directories_in_new_thread(suites: Vec<Suite>) {
-    spawn(move || cleanup_working_directories(suites.iter()));
+async fn run_cleanup_job(cancellation_token: CancellationToken, suites: Vec<Suite>) {
+    let mut clock = interval(Duration::from_secs(300));
+    loop {
+        let suites = suites.clone();
+        tokio::select! {
+            _ = clock.tick() => { }
+            _ = cancellation_token.cancelled() => { return }
+        };
+        spawn_blocking(move || cleanup_working_directories(suites.iter()));
+    }
 }
 
 fn wait_until_all_suites_have_terminated(suites: &[Suite]) {
