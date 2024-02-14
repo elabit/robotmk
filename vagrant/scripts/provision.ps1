2b2294cab561ae64e1c2dbc6043a1a01dc9ee500
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-02-14 16:17:47 +0100

    vagrant: remove oudated rcc provisioning

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -1,8 +1,3 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y --force googlechrome
 choco install -y --force python
-
-(new-object net.webclient).DownloadFile("https://nightly.link/elabit/robotmk/actions/artifacts/878920132.zip", "C:\Users\vagrant\Downloads\rcc.zip")
-Expand-Archive "C:\Users\vagrant\Downloads\rcc.zip" -Force -DestinationPath "C:\Users\vagrant\Downloads\"
-Copy-Item "C:\Users\vagrant\Downloads\windows64\rcc.exe" -Destination "C:\Users\vagrant\AppData\Local\Microsoft\WindowsApps\rcc.exe"
-rcc configure identity --do-not-track
