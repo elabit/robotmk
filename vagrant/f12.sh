f8ab678083b79e89a3be4a0a08e67b34cc54f4d3
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-02-15 14:42:20 +0100

    vagrant: make f12.sh independent of VirtualBox
    CMK-15654

--- vagrant/f12.sh
+++ vagrant/f12.sh
@@ -10,11 +10,16 @@ agentdir="C:/ProgramData/checkmk/agent"
 agentplugindir="$agentdir"/plugins
 agentconfigdir="$agentdir"/config
 agentbindir="$agentdir"/bin
+sshconfig="$topdir"/vagrant/ssh-config
+
+# Store ssh-config
+hostname=default
+vagrant ssh-config > "$sshconfig"
 
 # Sync rust binaries to vagrant machine (scheduler and agent plugin)
 (cd "$topdir"; cargo build --target=x86_64-pc-windows-gnu)
-sshpass -p "vagrant" scp -P 2222 "$topdir"/target/x86_64-pc-windows-gnu/debug/robotmk_scheduler.exe vagrant@127.0.0.1:"$agentbindir"
-sshpass -p "vagrant" scp -P 2222 "$topdir"/target/x86_64-pc-windows-gnu/debug/robotmk_agent_plugin.exe vagrant@127.0.0.1:"$agentplugindir"
+sshpass -p "vagrant" scp -F "$sshconfig" "$topdir"/target/x86_64-pc-windows-gnu/debug/robotmk_scheduler.exe vagrant@"$hostname":"$agentbindir"
+sshpass -p "vagrant" scp -F "$sshconfig" "$topdir"/target/x86_64-pc-windows-gnu/debug/robotmk_agent_plugin.exe vagrant@"$hostname":"$agentplugindir"
 
 # Sync config for agent plugin to vagrant machine
-sshpass -p "vagrant" scp -P 2222 "$topdir"/data/retry_rcc/windows.json vagrant@127.0.0.1:"$agentconfigdir"/robotmk.json
+sshpass -p "vagrant" scp -F "$sshconfig" "$topdir"/data/retry_rcc/windows.json vagrant@"$hostname":"$agentconfigdir"/robotmk.json
