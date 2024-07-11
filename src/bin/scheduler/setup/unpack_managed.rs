6870cf431f57f548e8395abd4405320c5059d863
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-07-11 08:15:32 +0200

    move permission handling to general setup
    Permission handling for managed robots was the only permission step not done
    during `GeneralSetup`.
    
    CMK-18212

--- src/bin/scheduler/setup/unpack_managed.rs
+++ src/bin/scheduler/setup/unpack_managed.rs
@@ -1,4 +1,3 @@
-use super::grant_full_access;
 use crate::internal_config::{Plan, Source};
 use camino::Utf8Path;
 use flate2::read::GzDecoder;
@@ -6,7 +5,6 @@ use log::info;
 use robotmk::lock::Locker;
 use robotmk::results::ManagementFailues;
 use robotmk::section::WriteSection;
-use robotmk::session::Session;
 use std::collections::HashMap;
 use std::fs::File;
 use tar::Archive;
@@ -41,42 +39,13 @@ fn unpack_setup(plans: Vec<Plan>) -> (Vec<Plan>, HashMap<String, String>) {
     (surviving_plans, failures)
 }
 
-fn permission_setup(plans: Vec<Plan>) -> (Vec<Plan>, HashMap<String, String>) {
-    let mut surviving_plans = Vec::new();
-    let mut failures = HashMap::new();
-    for plan in plans.into_iter() {
-        if let Session::User(user_session) = &plan.session {
-            if let Source::Managed { target, .. } = &plan.source {
-                if let Err(error) = grant_full_access(&user_session.user_name, target) {
-                    info!("{error:#}");
-                    failures.insert(plan.id.clone(), format!("{error:#}"));
-                    continue;
-                }
-                info!(
-                    "Adjusted permissions for {} for user `{}`.",
-                    target, &user_session.user_name
-                );
-            }
-        }
-        surviving_plans.push(plan);
-    }
-    (surviving_plans, failures)
-}
-
 pub fn setup(
     results_directory: &Utf8Path,
     results_directory_locker: &Locker,
     plans: Vec<Plan>,
 ) -> anyhow::Result<Vec<Plan>> {
     let (surviving_plans, unpack_failures) = unpack_setup(plans);
-    let (surviving_plans, permission_failures) = permission_setup(surviving_plans);
-    ManagementFailues(
-        unpack_failures
-            .into_iter()
-            .chain(permission_failures.into_iter())
-            .collect(),
-    )
-    .write(
+    ManagementFailues(unpack_failures).write(
         results_directory.join("management_failures.json"),
         results_directory_locker,
     )?;
