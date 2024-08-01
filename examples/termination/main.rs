cb682770b6218398b80e2472bd88b9865c45d004
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-08-01 10:32:34 +0200

    termination test: move check_tree_size

--- examples/termination/main.rs
+++ examples/termination/main.rs
@@ -1,71 +1,20 @@
+mod process_tree;
+
 use anyhow::Result as AnyhowResult;
 use camino::Utf8PathBuf;
 use clap::Parser;
+use process_tree::check_tree_size;
 use robotmk::config::RetryStrategy;
 use robotmk::environment::{Environment, RCCEnvironment, SystemEnvironment};
 use robotmk::plans::run_attempts_with_rebot;
 use robotmk::rf::robot::Robot;
 use robotmk::session::{CurrentSession, RunSpec, Session};
-use std::collections::{HashMap, HashSet};
 use std::env::var;
 use std::thread;
 use std::time::Duration;
-use sysinfo::{get_current_pid, Pid, Process, System};
+use sysinfo::{get_current_pid, System};
 use tempfile::tempdir;
 use tokio_util::sync::CancellationToken;
-
-fn get_children(processes: &HashMap<Pid, Process>, pid: &Pid) -> HashSet<Pid> {
-    processes
-        .iter()
-        .filter_map(|(child_pid, process)| {
-            ((process.parent().as_ref() == Some(pid)) && process.thread_kind().is_none())
-                .then_some(*child_pid)
-        })
-        .collect()
-}
-
-fn print_process_tree(depth: usize, processes: &HashMap<Pid, Process>, pid: Pid, max_depth: usize) {
-    let process = processes.get(&pid).unwrap();
-    println!(
-        "{}{pid} {:?} {:?} {:?}",
-        "-".repeat(depth),
-        process.status(),
-        process.name(),
-        process.exe(),
-    );
-    if depth >= max_depth {
-        return;
-    }
-    for child in get_children(processes, &pid) {
-        print_process_tree(depth + 1, processes, child, max_depth);
-    }
-}
-
-fn get_tree_size(processes: &HashMap<Pid, Process>, pid: Pid) -> usize {
-    #[cfg(windows)]
-    let mut count = {
-        let status = processes.get(&pid).unwrap().status();
-        use sysinfo::ProcessStatus;
-        match status {
-            ProcessStatus::Zombie => 0,
-            _ => 1,
-        }
-    };
-    #[cfg(unix)]
-    let mut count = 1;
-    for child in get_children(processes, &pid) {
-        count += get_tree_size(processes, child);
-    }
-    count
-}
-
-fn check_tree_size(system: &mut System, pid: Pid) -> usize {
-    system.refresh_processes();
-    let processes = system.processes();
-    println!("Process tree");
-    print_process_tree(0, processes, pid, 5);
-    get_tree_size(processes, pid)
-}
 
 #[derive(Parser)]
 struct Args {
