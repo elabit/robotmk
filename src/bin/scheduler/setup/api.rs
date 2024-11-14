dacc5cbee4a034bb8ed769db8acfa0cd546f695e
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-11-14 16:55:13 +0100

    Setup API: implement `run_steps`
    Our goal is to separate the installation planning (which is only based
    on the configuration) from the installation (which executes system
    calls, and can therefore fail in a multitude of ways.)
    
    During installation planning, the scheduler checks which plans will be
    affected by I/O steps and copies necessary configuration into a separate
    struct, which fully specify which I/O will take place. An API consumer
    must ensure that no plans are discarded during installation planning.
    
    CMK-20142

---
+++ src/bin/scheduler/setup/api.rs
@@ -0,0 +1,62 @@
+use crate::internal_config::Plan;
+use robotmk::results::SetupFailure;
+
+#[derive(Debug)]
+pub struct Error {
+    summary: String,
+    cause: anyhow::Error,
+}
+
+impl Error {
+    pub fn new(summary: String, cause: anyhow::Error) -> Error {
+        Error { summary, cause }
+    }
+}
+
+pub trait SetupStep {
+    fn setup(&self) -> Result<(), Error>;
+}
+
+pub type StepWithPlans = (Box<dyn SetupStep>, Vec<Plan>);
+
+struct SetupStepSuccess {}
+
+impl SetupStep for SetupStepSuccess {
+    fn setup(&self) -> Result<(), Error> {
+        Ok(())
+    }
+}
+
+pub fn skip(plans: Vec<Plan>) -> (Box<dyn SetupStep>, Vec<Plan>) {
+    (Box::new(SetupStepSuccess {}), plans)
+}
+
+pub fn run_steps(steps: Vec<StepWithPlans>) -> (Vec<Plan>, Vec<SetupFailure>) {
+    let mut plans = Vec::new();
+    let mut errors = Vec::new();
+    for (step, affected_plans) in steps.into_iter() {
+        match step.setup() {
+            Ok(()) => {
+                plans.extend(affected_plans);
+            }
+            Err(err) => {
+                for plan in &plans {
+                    log_plan_not_scheduled(&err.cause, plan, &err.summary);
+                    errors.push(SetupFailure {
+                        plan_id: plan.id.clone(),
+                        summary: err.summary.clone(),
+                        details: format!("{:?}", err.cause),
+                    });
+                }
+            }
+        }
+    }
+    (plans, errors)
+}
+
+pub fn log_plan_not_scheduled(error: &anyhow::Error, plan: &Plan, summary: &str) {
+    log::error!(
+        "Plan {}: {summary}. Plan won't be scheduled.\nCaused by: {error:?}",
+        plan.id
+    );
+}
